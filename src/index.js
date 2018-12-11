9d9c03186dc3899120a4f222b8b412e0b662b55a
MDomagala <marcin.domagala@silvair.com>
2018-12-11 10:53:10 +0100

    wip

--- src/index.js
+++ src/index.js
@@ -6,55 +6,74 @@ const config = require('./config.js');
 const Datastore = require('nedb-async-await').Datastore;
 const path = require('path');
 const createS3 = require('./s3');
+const fileUpload = require('express-fileupload');
 
-const users = Datastore({ filename: path.join('db', 'users.db'), autoload: true });
+const users = Datastore({filename: path.join('db', 'users.db'), autoload: true});
 
 const app = express();
-app.use(express.json());
+app.use(express.json({type: 'application/*+json'}));
+app.use(fileUpload({limits: {fileSize: 1 * 1024 * 1024}}));
 
 const ip = config.get('ip');
 const port = config.get('port');
-const s3Client = createS3();
-const file = 'object.txt';
+const s3Client = createS3('my-bucket-service-ex.2');
 
 app.get('/', (req, res) => {
-    res.statusCode = NOT_IMPLEMENTED;
-    res.send('work in progress');
+  res.statusCode = NOT_IMPLEMENTED;
+  res.send('work in progress');
+});
+
+app.get('/health', (req, res) => {
+  res.statusCode = OK;
+  res.send({});
 });
 
 app.route('/users')
-    .get(async (req, res) => {
-        let response;
-        try {
-            res.statusCode = OK;
-            response = await users.find({});
-        } catch (e) {
-            response = e;
-            res.statusCode = SERVICE_UNAVAILABLE;
-        }
-        return res.send(response);
-    })
-    .post(async (req, res) => {
-        console.log('post');
-        try {
-            const user = await users.insert(req.body);
-            res.statusCode = CREATED;
-            s3Client.putObject(file);
-            res.send(user._id);
-        } catch (e) {
-            res.statusCode = SERVICE_UNAVAILABLE;
-            res.send(e);
-        }
-    });
-
-app.delete('/users/:userId', async (req, res) => {
+  .get(async (req, res) => {
+    let response;
     try {
-        res.statusCode = await users.remove({_id: req.params.userId}) === 1 ? NO_CONTENT : NOT_FOUND;
-        res.send('');
+      res.statusCode = OK;
+      response = await users.find({});
     } catch (e) {
-        res.statusCode = SERVICE_UNAVAILABLE;
-        res.send(e);
+      response = e;
+      res.statusCode = SERVICE_UNAVAILABLE;
     }
-});
+    return res.send(response);
+  })
+  .post(async (req, res) => {
+    try {
+      let entry = {...req.body};
+      if (req.files) {
+        await s3Client.putObject(req.files.file.data, req.files.file.name);
+        entry.avatar = req.files.file.name;
+      }
+      const user = await users.insert(entry);
+      res.statusCode = CREATED;
+      res.send(user._id);
+    } catch (e) {
+      res.statusCode = SERVICE_UNAVAILABLE;
+      res.send(e);
+    }
+  });
+
+app.route('/users/:userId')
+  .get(async (req, res) => {
+    const user = await users.findOne({_id: req.params.userId});
+    let img;
+    if (user.avatar) {
+      img = await s3Client.getUrl(user.avatar);
+    }
+    user.img = img;
+    res.send(user);
+  })
+  .delete(async (req, res) => {
+    try {
+      res.statusCode = await users.remove({_id: req.params.userId}) === 1 ? NO_CONTENT : NOT_FOUND;
+      res.send('');
+    } catch (e) {
+      res.statusCode = SERVICE_UNAVAILABLE;
+      res.send(e);
+    }
+  });
 
 app.listen(port, ip, () => console.log(`running on http://${ip}:${port}!`));
--- src/s3.js
+++ src/s3.js
@@ -1,28 +1,41 @@
 const AWS = require('aws-sdk');
-const fs = require('fs');
-const path = require('path');
 
-module.exports = function createS3() {
-    const s3 = new AWS.S3();
-    const myBucket = 'my-bucket-service';
+module.exports = function createS3(bucket) {
+  const s3 = new AWS.S3();
+  const Bucket = bucket;
 
-    async function putObject(filePath) {
-        try {
-            const file = await fs.readFileSync(filePath);
-            const data = new Buffer(file);
-            s3.putObject({
-                Bucket: myBucket,
-                Key: path.basename(filePath),
-                Body: data,
-                ACL: 'public-read'
-            }, function (response) {
-                console.log(response);
-            });
-        } catch (e) {
-            console.log(e);
+  async function putObject(file, filename) {
+    return new Promise((resolve, reject) => {
+      s3.putObject({
+        Bucket,
+        Key: filename,
+        Body: file,
+        ACL: 'public-read'
+      }, function (err, data) {
+        if (err) {
+          reject(err);
         }
-    }
-  return {putObject};
+        resolve(data);
+      });
+    })
+  }
+
+  async function getUrl(Key) {
+    return s3.getSignedUrl('getObject', {Bucket, Key})
+  }
+
+  async function getObject(Key) {
+    return new Promise((resolve, reject) => {
+      s3.getObject({Bucket, Key}, function (err, data) {
+        if (err) {
+          reject(err);
+        }
+        resolve(data);
+      })
+    })
+  }
+
+  return {putObject, getUrl, getObject};
 };
 
 
